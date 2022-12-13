b7a700555433c69aa276652038a1f6e6d5a1c291
Viktor Shevchuk <Viktor.Shevchuk@infopulse.com>
2022-12-14 01:21:32 +0200

    1

---
+++ src/babyjub/eddsa.ts
@@ -0,0 +1,96 @@
+// @ts-ignore: blake-hash no declarations
+import createBlakeHash from 'blake-hash';
+import { Hex } from '../hex';
+import { babyJub, BabyJub } from './babyjub';
+import { poseidon } from '../poseidon';
+import { F1Field, Scalar, utils } from '../ff';
+import { PublicKey, Signature } from './eddsa-keys';
+
+export class Eddsa {
+  babyJub: BabyJub = babyJub;
+  F = babyJub.F;
+
+  static pruneBuffer(buff: Uint8Array) {
+    buff[0] = buff[0] & 0xf8;
+    buff[31] = buff[31] & 0x7f;
+    buff[31] = buff[31] | 0x40;
+    return buff;
+  }
+
+  static prv2pub(prv: Uint8Array): [bigint, bigint] {
+    // const F = babyJub.F;
+    const privHex = Hex.encodeString(prv);
+    const sBuff = this.pruneBuffer(createBlakeHash('blake512').update(privHex, 'hex').digest());
+
+    const s = Scalar.fromRprLE(sBuff, 0, 32);
+    const A = babyJub.mulPointEscalar(babyJub.Base8, Scalar.shr(s, 3));
+    return A;
+  }
+
+  static signPoseidon(prv: Uint8Array, msg: bigint) {
+    const privateHex = Hex.encodeString(prv);
+    const h1 = createBlakeHash('blake512').update(privateHex, 'hex').digest();
+
+    const sBuff = Eddsa.pruneBuffer(h1.slice(0, 32));
+    const s = utils.leBuff2int(sBuff);
+    const A = babyJub.mulPointEscalar(babyJub.Base8, Scalar.shr(s, 3));
+
+    const msgBuff = utils.leInt2Buff(msg, 32);
+
+    const composeBuff = new Uint8Array(64);
+    composeBuff.set(h1.slice(32, 64), 0);
+    composeBuff.set(msgBuff, 32);
+
+    const rBuff = createBlakeHash('blake512').update(Hex.encodeString(composeBuff), 'hex').digest();
+    let r = utils.leBuff2int(rBuff);
+    const Fr = new F1Field(babyJub.subOrder);
+    r = Fr.e(r);
+    const R8 = babyJub.mulPointEscalar(babyJub.Base8, r);
+    const hm = poseidon.hash([R8[0], R8[1], A[0], A[1], msg]);
+    const S = Fr.add(r, Fr.mul(hm, s));
+    return {
+      R8: R8,
+      S: S
+    };
+  }
+
+  static verifyPoseidon(msg: bigint, sig: Signature, A: PublicKey['p']): boolean {
+    // Check parameters
+    if (typeof sig != 'object') return false;
+    if (!Array.isArray(sig.R8)) return false;
+    if (sig.R8.length != 2) return false;
+    if (!babyJub.inCurve(sig.R8)) return false;
+    if (!Array.isArray(A)) return false;
+    if (A.length != 2) return false;
+    if (!babyJub.inCurve(A)) return false;
+    if (sig.S >= babyJub.subOrder) return false;
+
+    const hm = poseidon.hash([sig.R8[0], sig.R8[1], A[0], A[1], msg]);
+    const hms = Scalar.e(babyJub.F.toObject(hm));
+
+    const Pleft = babyJub.mulPointEscalar(babyJub.Base8, sig.S);
+    let Pright = babyJub.mulPointEscalar(A, Scalar.mul(hms, 8));
+    Pright = babyJub.addPoint(sig.R8, Pright);
+
+    if (!babyJub.F.eq(Pleft[0], Pright[0])) return false;
+    if (!babyJub.F.eq(Pleft[1], Pright[1])) return false;
+    return true;
+  }
+
+  static packSignature(sig: Signature): Uint8Array {
+    const buff = new Uint8Array(64);
+    const R8p = babyJub.packPoint(sig.R8);
+    buff.set(R8p, 0);
+    Scalar.toRprLE(buff, 32, sig.S, 32);
+    return buff;
+  }
+
+  static unpackSignature(sigBuff: Uint8Array) {
+    return {
+      R8: babyJub.unpackPoint(sigBuff.slice(0, 32)),
+      S: Scalar.fromRprLE(sigBuff, 32, 32)
+    };
+  }
+}
+
+export const eddsa = Eddsa;
---
+++ src/poseidon/poseidon-opt.ts
@@ -0,0 +1,113 @@
+import { F1Field, Scalar, utils } from '../ff';
+import op from './poseidon-constants-opt.json';
+
+const N_ROUNDS_F = 8;
+const N_ROUNDS_P = [56, 57, 56, 60, 60, 63, 64, 63, 60, 66, 60, 65, 70, 60, 64, 68];
+const SPONGE_INPUTS = 16;
+const SPONGE_CHUNK_SIZE = 31;
+
+const F = new F1Field(
+  Scalar.fromString('21888242871839275222246405745257275088548364400416034343698204186575808495617')
+);
+const pow5 = (a: bigint): bigint => F.mul(a, F.square(F.square(a, a)));
+
+// circomlibjs Poseidon bn128
+export class Poseidon {
+  static F = F;
+  static hash(inputs: bigint[]): bigint {
+    if (!(inputs.length > 0 && inputs.length <= N_ROUNDS_P.length)) {
+      throw new Error('Invalid inputs');
+    }
+
+    const opt = utils.unstringifyBigInts(op);
+
+    const t = inputs.length + 1;
+    const nRoundsF = N_ROUNDS_F;
+    const nRoundsP = N_ROUNDS_P[t - 2];
+    const C = opt.C[t - 2];
+    const S = opt.S[t - 2];
+    const M = opt.M[t - 2];
+    const P = opt.P[t - 2];
+
+    let state: bigint[] = [F.zero, ...inputs.map((a) => F.e(a))];
+
+    state = state.map((a, i) => F.add(a, C[i]));
+
+    for (let r = 0; r < nRoundsF / 2 - 1; r++) {
+      state = state.map((a) => pow5(a));
+      state = state.map((a, i) => F.add(a, C[(r + 1) * t + i]));
+      state = state.map((_, i) =>
+        state.reduce((acc, a, j) => F.add(acc, F.mul(M[j][i], a)), F.zero)
+      );
+    }
+    state = state.map((a) => pow5(a));
+    state = state.map((a, i) => F.add(a, C[(nRoundsF / 2 - 1 + 1) * t + i]));
+    state = state.map((_, i) => state.reduce((acc, a, j) => F.add(acc, F.mul(P[j][i], a)), F.zero));
+    for (let r = 0; r < nRoundsP; r++) {
+      state[0] = pow5(state[0]);
+      state[0] = F.add(state[0], C[(nRoundsF / 2 + 1) * t + r]);
+
+      const s0 = state.reduce((acc, a, j) => {
+        return F.add(acc, F.mul(S[(t * 2 - 1) * r + j], a));
+      }, F.zero);
+      for (let k = 1; k < t; k++) {
+        state[k] = F.add(state[k], F.mul(state[0], S[(t * 2 - 1) * r + t + k - 1]));
+      }
+      state[0] = s0;
+    }
+    for (let r = 0; r < nRoundsF / 2 - 1; r++) {
+      state = state.map((a) => pow5(a));
+      state = state.map((a, i) => F.add(a, C[(nRoundsF / 2 + 1) * t + nRoundsP + r * t + i]));
+      state = state.map((_, i) =>
+        state.reduce((acc, a, j) => F.add(acc, F.mul(M[j][i], a)), F.zero)
+      );
+    }
+    state = state.map((a) => pow5(a));
+    state = state.map((_, i) => state.reduce((acc, a, j) => F.add(acc, F.mul(M[j][i], a)), F.zero));
+
+    return F.normalize(state[0]);
+  }
+
+  static hashBytes(msg: Uint8Array): bigint {
+    const inputs = new Array(SPONGE_INPUTS).fill(BigInt(0));
+    let dirty = false;
+    let hash: bigint;
+
+    let k = 0;
+    for (let i = 0; i < parseInt(`${msg.length / SPONGE_CHUNK_SIZE}`); i += 1) {
+      dirty = true;
+      inputs[k] = utils.beBuff2int(msg.slice(SPONGE_CHUNK_SIZE * i, SPONGE_CHUNK_SIZE * (i + 1)));
+      if (k === SPONGE_INPUTS - 1) {
+        hash = Poseidon.hash(inputs);
+        dirty = false;
+        inputs[0] = hash.valueOf();
+        inputs.fill(BigInt(0), 1, SPONGE_CHUNK_SIZE);
+        for (let j = 1; j < SPONGE_INPUTS; j += 1) {
+          inputs[j] = BigInt(0);
+        }
+        k = 1;
+      } else {
+        k += 1;
+      }
+    }
+
+    if (msg.length % SPONGE_CHUNK_SIZE != 0) {
+      const buff = new Uint8Array(SPONGE_CHUNK_SIZE);
+      const slice = msg.slice(parseInt(`${msg.length / SPONGE_CHUNK_SIZE}`) * SPONGE_CHUNK_SIZE);
+      slice.forEach((v, idx) => {
+        buff[idx] = v;
+      });
+      inputs[k] = utils.beBuff2int(buff);
+      dirty = true;
+    }
+
+    if (dirty) {
+      // we haven't hashed something in the main sponge loop and need to do hash here
+      hash = Poseidon.hash(inputs);
+    }
+
+    // @ts-ignore: if we reach here then hash should be assigned value
+    return hash.valueOf();
+  }
+}
+export const poseidon = Poseidon;
